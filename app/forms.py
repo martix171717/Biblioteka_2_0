4ce95841ea5b091d1821950da8242c0f642cd0d4
Martyna Szymańska <martix171717@gmail.com>
2021-11-05 14:57:31 +0100

    Add forms

---
+++ app/forms.py
@@ -0,0 +1,38 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SelectField
+from wtforms.validators import DataRequired
+from app.models import Book, Author
+
+
+class BooksForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    year = IntegerField('Publication Year')
+    author = SelectField('Choose an author')
+
+    @classmethod
+    def new(cls):
+        # Instantiate the form
+        form = cls()
+        # Update the choices for the author field
+        form.author.choices = [x.name for x in Author.query.all()]
+        return form
+
+
+class AuthorForm(FlaskForm):
+    name = StringField('Author', validators=[DataRequired()])
+
+
+class BorrowingForm(FlaskForm):
+    title = SelectField('Choose a title')
+    date = StringField('Date of borrowing, yyyy-mm-dd', validators=[DataRequired()])
+    where = StringField('Who borrowed', validators=[DataRequired()])
+
+    @classmethod
+    def new(cls):
+        form = cls()
+        form.title.choices = [x.title for x in Book.query.all()]
+        return form
+
+
+class DeleteForm(FlaskForm):
+    id = IntegerField('Id', validators=[DataRequired()])
